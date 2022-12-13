7c30eed753dfde78359d8c34a8ebcfa8b0458edd
Thang Le <ledinhthang102@gmail.com>
2022-12-13 16:38:37 +0700

    test header

--- getotp.php
+++ getotp.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
